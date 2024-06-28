a6a0f784473de7f2f04923ea6ea9807488d336b0
maya745 <maya4maple@gmail.com>
2024-06-28 06:32:15 -0400

    Update CircleDraw.c

--- CircleDraw.c
+++ CircleDraw.c
@@ -10,9 +10,10 @@ int main() {
 
     float theta;
 
-    void pointOnCircle(float theta) {  //A function giving point coordinate
-        xCoord = radius * cos(theta); //value of x-coordinate. i is theta and increases over time
-        yCoord = radius * sin(theta); //value of y-coordinate. Similar to x-coord
+    //function will calculate coordinates and print them
+    void pointOnCircle(float theta) {
+        xCoord = radius * cos(theta);     //value of x-coordinate. theta will increase over time
+        yCoord = radius * sin(theta);     //value of y-coordinate. Similar to x-coord
         printf("(%.2f, %.2f)\n", xCoord, yCoord);
 
     }
@@ -20,7 +21,7 @@ int main() {
     printf("Points on a circle of radius %d\n", radius);
 
     for (theta = 0; theta <= 2 * pi; theta += 0.1) { //for loop unitl finish circle
-      pointOnCircle(theta);           //call function that prints points
+      pointOnCircle(theta);                          //call function that prints points
     }
 
       return 0;
