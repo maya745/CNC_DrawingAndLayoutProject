32f1f44dd35235c2aa559ba59076dee4c999a193
maya745 <maya4maple@gmail.com>
2024-06-28 08:07:42 -0400

    Add files via upload

---
+++ arrayRectangleDraw.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+struct Coordinates { 
+    //create members
+    float xCoord;       
+    float yCoord;
+};
+
+int main() {
+
+    //create length and width variables
+    float maxWidth;
+    float maxLength;
+
+    //get rectangle dimensions from user
+    printf("What is the length of the rectangle?  \n");
+    scanf("%f", &maxLength);
+
+    printf("What is the width of the rectangle?  \n");
+    scanf("%f", &maxWidth);
+    //now we have a maximum length and width
+
+    int vertices = 4;
+
+    struct Coordinates coordArray[vertices];
+
+    //use arrays for each coordinate
+    coordArray[0].xCoord = 0;
+    coordArray[0].yCoord = 0;
+
+    coordArray[1].xCoord = 0;
+    coordArray[1].yCoord = maxWidth;
+
+    coordArray[2].xCoord = maxLength;
+    coordArray[2].yCoord = maxWidth;
+
+    coordArray[3].xCoord = maxLength;
+    coordArray[3].yCoord = 0;
+
+    for (int i = 0; i < vertices; i++) { //create for loop to print out the coordinate values
+        printf("(%.2f, %.2f)\n", coordArray[i].xCoord, coordArray[i].yCoord);
+    }
+
+    printf("(0, 0) \n");        //the last point should be repeated to finish the shape
+
+    return 0;
+}
