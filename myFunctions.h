4f521cd9316a94f13a5c09b4e3e21483febdf196
maya745 <maya4maple@gmail.com>
2024-06-28 21:30:17 -0400

    Update myFunctions.h

--- myFunctions.h
+++ myFunctions.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h> // For math
+#include <windows.h> // For sleeping
 
 struct Coordinates { 
     //create members
@@ -8,7 +9,14 @@ struct Coordinates {
 };
 
 void printCoordArray(struct Coordinates Array[], int vrtcs) {
+    
+    Sleep(100);
+    printf("Okay. Here's your points:\n");
+
+    Sleep(100);
        for (int i = 0; i < vrtcs; i++) {        //create for loop to print out the coordinate values
         printf("(%.2f, %.2f)\n", Array[i].xCoord, Array[i].yCoord);
     }
+
+    printf("(0.00, 0.00)  \n");        //the last point should be repeated to finish the shape
 }
